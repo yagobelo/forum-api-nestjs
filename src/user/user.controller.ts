c7c2a88dd737f24580bcd9eb3eb297d7ad9671fd
yagobelo <yago.belo00@gmail.com>
2024-06-22 21:21:42 -0300

    feat: adicionado rotas crud

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { Prisma, User as UserModel } from '@prisma/client';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Post('signup')
+  async signupUser(
+    @Body() userData: Prisma.UserCreateInput,
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
+  }
+}
