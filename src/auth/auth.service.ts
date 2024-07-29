91ea64df0445c9cf1a63052c64dce530bf2612bb
yagobelo <yago.belo00@gmail.com>
2024-07-29 10:21:41 -0300

    feat: adicionado login com autenticação

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Inject,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  @Inject()
+  private readonly userService: UserService;
+
+  async signin(
+    params: Prisma.UserCreateInput,
+  ): Promise<Omit<User, 'password'>> {
+    const user = await this.userService.user({ email: params.email });
+    if (!user) throw new NotFoundException('User not found');
+    const passwordMatch = await bcrypt.compare(params.password, user.password);
+    if (!passwordMatch) throw new UnauthorizedException('Invalid credentials');
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password, ...result } = user;
+
+    return result;
+  }
+}
