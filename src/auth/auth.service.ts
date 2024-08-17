f14b5fda4da084f5ea8271bbef1bab3b857bc33c
yagobelo <yago.belo00@gmail.com>
2024-08-17 13:27:20 -0300

    feat: add jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,26 +4,29 @@ import {
   NotFoundException,
   UnauthorizedException,
 } from '@nestjs/common';
-import { Prisma, User } from '@prisma/client';
+import { Prisma } from '@prisma/client';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   @Inject()
   private readonly userService: UserService;
 
+  @Inject()
+  private readonly jwtService: JwtService;
+
   async signin(
     params: Prisma.UserCreateInput,
-  ): Promise<Omit<User, 'password'>> {
+  ): Promise<{ access_token: string }> {
     const user = await this.userService.user({ email: params.email });
     if (!user) throw new NotFoundException('User not found');
     const passwordMatch = await bcrypt.compare(params.password, user.password);
     if (!passwordMatch) throw new UnauthorizedException('Invalid credentials');
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { password, ...result } = user;
+    const payload = { sub: user.id };
 
-    return result;
+    return { access_token: await this.jwtService.signAsync(payload) };
   }
 }
