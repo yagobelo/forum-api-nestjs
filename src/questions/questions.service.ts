6c5b2888c5c3c3af9da625d0208cf3df723b190e
yagobelo <yago.belo00@gmail.com>
2024-09-19 14:59:58 -0300

    feat: ep12

--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -8,9 +8,9 @@ export class QuestionsService {
   @Inject()
   private readonly prisma: PrismaService;
 
-  async create(createQuestionDto: CreateQuestionDto, userId: number) {
+  async create(createQuestionDto: CreateQuestionDto, req: any) {
     return await this.prisma.questions.create({
-      data: { ...createQuestionDto, userId },
+      data: { ...createQuestionDto, userId: req.sub.sub },
     });
   }
 
