4c5af9cfeb9c9e8b0a38bb6bc3d475364c0b1141
yagobelo <yago.belo00@gmail.com>
2024-09-21 16:56:14 -0300

    feat: ep 13

--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -15,11 +15,32 @@ export class QuestionsService {
   }
 
   async findAll() {
-    return await this.prisma.questions.findMany();
+    return await this.prisma.questions.findMany({
+      include: {
+        answers: true,
+        user: {
+          select: {
+            name: true,
+            email: true,
+          },
+        },
+      },
+    });
   }
 
   async findOne(id: number) {
-    return await this.prisma.questions.findUnique({ where: { id } });
+    return await this.prisma.questions.findUnique({
+      where: { id },
+      include: {
+        answers: true,
+        user: {
+          select: {
+            name: true,
+            email: true,
+          },
+        },
+      },
+    });
   }
 
   async update(id: number, updateQuestionDto: UpdateQuestionDto) {
