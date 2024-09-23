bc7936809024caec1fec65df5728920744894e36
yagobelo <yago.belo00@gmail.com>
2024-09-23 18:36:19 -0300

    feat: add validation min in crate user in ep15

--- src/user/dto/createUser.dto.ts
+++ src/user/dto/createUser.dto.ts
@@ -1,15 +1,23 @@
-import { IsAlpha, IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsAlpha,
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+} from 'class-validator';
 
 export class CreateUserDto {
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @MinLength(3)
   @IsString()
   @IsNotEmpty()
   name: string;
 
   @IsAlpha()
+  @MinLength(6)
   @IsNotEmpty()
   password: string;
 }
